692a77f8506c502adbe04674224966d3e840a42e
prostfk <prostfk@gmail.com>
2018-08-20 19:41:31 +0300

    init commit

---
+++ src/main/java/by/bntu/info/BtnuWebSiteApplication.java
@@ -0,0 +1,12 @@
+package by.bntu.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtnuWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtnuWebSiteApplication.class, args);
+    }
+}
